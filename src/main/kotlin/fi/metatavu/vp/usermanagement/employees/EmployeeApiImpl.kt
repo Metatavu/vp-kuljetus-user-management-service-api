34bad9c67cc6c33c31668eeddc83776c2a0ee163
Katja Danilova <katja.danilova@protonmail.com>
2024-07-09 17:28:44 +0300

    Employee update fix

--- src/main/kotlin/fi/metatavu/vp/usermanagement/employees/EmployeeApiImpl.kt
+++ src/main/kotlin/fi/metatavu/vp/usermanagement/employees/EmployeeApiImpl.kt
@@ -67,7 +67,11 @@ class EmployeeApiImpl: EmployeesApi, AbstractApi() {
 
     @RolesAllowed(MANAGER_ROLE)
     override fun updateEmployee(employeeId: UUID, employee: Employee): Uni<Response> = CoroutineScope(vertx.dispatcher()).async {
-        usersController.findEmployeeNumberDuplicate(employee.employeeNumber).let { if (it.isNotEmpty()) return@async createBadRequest("Employee number already exists")}
+        usersController.findEmployeeNumberDuplicate(employee.employeeNumber).let {
+            if (it.isNotEmpty() && it.find { d -> d.id == employeeId.toString() } == null) return@async createBadRequest(
+                "Employee number already exists"
+            )
+        }
         val found = usersController.find(employeeId, EMPLOYEE_ROLE) ?: return@async createNotFound("Employee not found")
         if (found.enabled == false && employee.archivedAt != null) return@async createBadRequest("Cannot update archived employee")
         val updated = usersController.updateEmployee(found, employee) ?: return@async createInternalServerError("Failed updating a user")
