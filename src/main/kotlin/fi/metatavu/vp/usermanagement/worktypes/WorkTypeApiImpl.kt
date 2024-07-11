e5b811d6df8e89b79805d16b0fa30448fce95b7f
Antti Leppä <antti.leppa@metatavu.fi>
2024-07-11 10:37:09 +0300

    Changed APIs to use withCoroutineScope pattern

--- src/main/kotlin/fi/metatavu/vp/usermanagement/worktypes/WorkTypeApiImpl.kt
+++ src/main/kotlin/fi/metatavu/vp/usermanagement/worktypes/WorkTypeApiImpl.kt
@@ -8,24 +8,16 @@ import fi.metatavu.vp.usermanagement.timeentries.TimeEntryController
 import io.quarkus.hibernate.reactive.panache.common.WithSession
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction
 import io.smallrye.mutiny.Uni
-import io.smallrye.mutiny.coroutines.asUni
-import io.vertx.kotlin.coroutines.dispatcher
 import jakarta.annotation.security.RolesAllowed
 import jakarta.enterprise.context.RequestScoped
 import jakarta.inject.Inject
 import jakarta.ws.rs.core.Response
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.async
 import java.util.*
 
 @RequestScoped
 @WithSession
-@OptIn(ExperimentalCoroutinesApi::class)
+@Suppress("unused")
 class WorkTypeApiImpl : WorkTypesApi, AbstractApi() {
-
-    @Inject
-    lateinit var vertx: io.vertx.core.Vertx
 
     @Inject
     lateinit var workTypeController: WorkTypeController
@@ -37,39 +29,39 @@ class WorkTypeApiImpl : WorkTypesApi, AbstractApi() {
     lateinit var timeEntryController: TimeEntryController
 
     @RolesAllowed(MANAGER_ROLE, EMPLOYEE_ROLE, DRIVER_ROLE)
-    override fun listWorkTypes(category: WorkTypeCategory?): Uni<Response> = CoroutineScope(vertx.dispatcher()).async {
+    override fun listWorkTypes(category: WorkTypeCategory?): Uni<Response> = withCoroutineScope({
         val (workTypes, count) = workTypeController.list(category)
         createOk(workTypeTranslator.translate(workTypes), count)
-    }.asUni()
+    })
 
     @WithTransaction
     @RolesAllowed(MANAGER_ROLE)
-    override fun createWorkType(workType: WorkType): Uni<Response> = CoroutineScope(vertx.dispatcher()).async {
+    override fun createWorkType(workType: WorkType): Uni<Response> = withCoroutineScope({
         val duplicates = workTypeController.find(workType.name, workType.category)
         if (duplicates != null) {
-            return@async createConflict(
+            return@withCoroutineScope createConflict(
                 "Work type with name ${workType.name} and category ${workType.category} already exists"
             )
         }
         val createdWorkType = workTypeController.create(workType = workType)
         createCreated(workTypeTranslator.translate(createdWorkType))
-    }.asUni()
+    })
 
     @RolesAllowed(MANAGER_ROLE)
-    override fun findWorkType(workTypeId: UUID): Uni<Response> = CoroutineScope(vertx.dispatcher()).async {
-        val found = workTypeController.find(workTypeId) ?: return@async createNotFound(
+    override fun findWorkType(workTypeId: UUID): Uni<Response> = withCoroutineScope({
+        val found = workTypeController.find(workTypeId) ?: return@withCoroutineScope createNotFound(
             createNotFoundMessage(
                 WORK_TYPE,
                 workTypeId
             )
         )
         createOk(workTypeTranslator.translate(found))
-    }.asUni()
+    })
 
     @WithTransaction
     @RolesAllowed(MANAGER_ROLE)
-    override fun deleteWorkType(workTypeId: UUID): Uni<Response> = CoroutineScope(vertx.dispatcher()).async {
-        val found = workTypeController.find(workTypeId) ?: return@async createNotFound(
+    override fun deleteWorkType(workTypeId: UUID): Uni<Response> = withCoroutineScope({
+        val found = workTypeController.find(workTypeId) ?: return@withCoroutineScope createNotFound(
             createNotFoundMessage(
                 WORK_TYPE,
                 workTypeId
@@ -77,11 +69,12 @@ class WorkTypeApiImpl : WorkTypesApi, AbstractApi() {
         )
         val timeEntries = timeEntryController.listByWorkType(workType = found)
         if (timeEntries > 0) {
-            return@async createConflict(
+            return@withCoroutineScope createConflict(
                 "Work type with id $workTypeId is in use in time entries"
             )
         }
         workTypeController.delete(found)
         createNoContent()
-    }.asUni()
+    })
+
 }
