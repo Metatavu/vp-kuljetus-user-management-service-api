dbcd65410708a6ef0c12f5f603b514f806a97dfe
Katja Danilova <katja.danilova@protonmail.com>
2024-08-14 00:46:02 +0300

    manually disable test endpoint

--- src/test/kotlin/fi/metatavu/vp/usermanagement/settings/RabbitMQTestProfile.kt
+++ src/test/kotlin/fi/metatavu/vp/usermanagement/settings/RabbitMQTestProfile.kt
@@ -24,8 +24,6 @@ class RabbitMQTestProfile: QuarkusTestProfile {
         config["mp.messaging.outgoing.vp-out.connector"]="smallrye-rabbitmq"
         config["mp.messaging.outgoing.vp-out.exchange.name"]="test-exchange"
 
-        config["quarkus.test.native-image-profile"]="test"   // manually set the profile so that in native tests the TestEndpoint is activated by "test" profile
-                                                             // https://github.com/quarkusio/quarkus/issues/7551#issuecomment-594424341
         config["env"] = "TEST"
         return config
     }
